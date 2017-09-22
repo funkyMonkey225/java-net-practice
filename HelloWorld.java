fe141a27b6dbb1d21b569c265a64cadc766793cb
Mac User <sabbey37@gmail.com>
2017-09-22 09:56:16 -0400

    Adds meh class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,20 @@
 class HelloWorld {
-    public static void main(String)
+    // Everything is a class in JavaScript, only methods(no functions)
+    public static void main(String [] args) {
+        System.out.println("Hello Java");
+    }
 }
+
+class Meh {
+    public static void meh(String wha) {
+        System.out.println(wha);
+    }
+
+    public static void main(String [] args) {
+        Meh.meh("yeah?");
+    }
+}
+
+// Java compiler looks for a .class file and tries to load main method
+// javac HelloWorld.java
+// java HelloWorld
