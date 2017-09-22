b6e27a9e89a0a4859dcefa3c18c90a4a94b8093a
Mac User <sabbey37@gmail.com>
2017-09-22 10:00:20 -0400

    Adds method to meh class

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,11 +10,17 @@ class Meh {
         System.out.println(wha);
     }
 
+    public static void meh(String wha, String who) {
+        System.out.println(wha + " " + who);
+    }
+
     public static void main(String [] args) {
         Meh.meh("yeah?");
+        Meh.meh("hey", "you");
     }
 }
 
+// Can have different versions of same method based on arguments
 // Java compiler looks for a .class file and tries to load main method
 // javac HelloWorld.java
 // java HelloWorld
