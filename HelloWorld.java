f6753f6431196ae444ca7d4c00633600ffc010c8
Mac User <sabbey37@gmail.com>
2017-09-22 10:04:35 -0400

    Adds beez method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,14 @@
-class HelloWorld {
-    // Everything is a class in JavaScript, only methods(no functions)
-    public static void main(String [] args) {
-        System.out.println("Hello Java");
-    }
-}
-
 class Meh {
     public static void meh(String wha) {
         System.out.println(wha);
     }
-
+    // Static means it belongs to class, not instance
     public static void meh(String wha, String who) {
         System.out.println(wha + " " + who);
+    }
+
+    public void beezz() {
+        System.out.println("beezzzzz");
     }
 
     public static void main(String [] args) {
@@ -20,7 +17,17 @@ class Meh {
     }
 }
 
+class HelloWorld {
+    // Everything is a class in JavaScript, only methods(no functions)
+    public static void main(String [] args) {
+        System.out.println("Hello Java");
+        Meh myMeh = new Meh();
+        myMeh.beezz();
+    }
+}
+
 // Can have different versions of same method based on arguments
 // Java compiler looks for a .class file and tries to load main method
 // javac HelloWorld.java
 // java HelloWorld
+// rm *.class (deletes all class files)
